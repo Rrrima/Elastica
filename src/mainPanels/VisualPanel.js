48446b8c11009b8e4a9ebf8f1ba7c3a0bb293560
anuzk13 <am.cardenas927@gmail.com>
2023-02-28 12:31:17 -0800

    Add frabic JS to the project and test the canvas

--- src/mainPanels/VisualPanel.js
+++ src/mainPanels/VisualPanel.js
@@ -4,10 +4,25 @@ import Stack from "@mui/material/Stack";
 import VideocamIcon from "@mui/icons-material/Videocam";
 // import MicIcon from "@mui/icons-material/Mic";
 import AutoGraphIcon from "@mui/icons-material/AutoGraph";
+import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
 
 export default function VisualPanel() {
+
+  const { editor, onReady } = useFabricJSEditor()
+  const onAddCircle = () => {
+    editor?.addCircle()
+  }
+  const onAddRectangle = () => {
+    editor?.addRectangle()
+  }
+
   return (
     <div className="main-panel" id="visual-panel">
+      <div>
+        <button onClick={onAddCircle}>Add circle</button>
+        <button onClick={onAddRectangle}>Add Rectangle</button>
+        <FabricJSCanvas className="canvas-panel" onReady={onReady} />
+      </div>
       <div className="bottom right">
         <Stack direction="row">
           <IconButton aria-label="videocam">
