853dc83d35633b15ac4a1c9a11440ebc449c546f
Rima Yining Cao <32640767+Rrrima@users.noreply.github.com>
2023-03-01 21:14:50 -0800

    Canvas (#3)
    * Add frabic JS to the project and test the canvas
    
    * Add background image
    
    * Add objects just once and use local file
    
    * Add text box
    
    * Add lottie animations to canvas
    
    * Use local lottie files instead of URL
    
    ---------
    
    Co-authored-by: anuzk13 <am.cardenas927@gmail.com>

--- src/mainPanels/VisualPanel.js
+++ src/mainPanels/VisualPanel.js
@@ -4,10 +4,57 @@ import Stack from "@mui/material/Stack";
 import VideocamIcon from "@mui/icons-material/Videocam";
 // import MicIcon from "@mui/icons-material/Mic";
 import AutoGraphIcon from "@mui/icons-material/AutoGraph";
+import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
+import { fabric } from "fabric";
+import Stitch from "../resources/Images/stitch.png";
+import LottieFabric from "../LottieFabric";
+import AnimatedHeart1 from "../resources/lotties/heart1.json";
+
+import { objectDict } from "../resources/ObjectDict";
 
 export default function VisualPanel() {
+
+  const { editor, onReady } = useFabricJSEditor()
+
+  React.useEffect(() => {
+    if (!editor || !fabric || !editor.canvas.isEmpty()) {
+      return;
+    }
+    fabric.Image.fromURL(
+      Stitch,
+      (image) => {
+        const obj = editor.canvas.add(image);
+        console.log(obj)
+      }
+    );
+    const textbox = new fabric.Textbox('Ohana means family', {
+      left: 50,
+      top: 50,
+      width: 150,
+      fontSize: 20,
+      fontFamily:'Impact'
+    });
+    editor.canvas.add(textbox);
+    const fabricImage = new LottieFabric(AnimatedHeart1, {
+          scaleX: 1,
+        })
+    editor.canvas.add(fabricImage)
+  }, [editor?.canvas.isEmpty()])
+
+  const onAddCircle = () => {
+    editor?.addCircle()
+  }
+  const onAddRectangle = () => {
+    editor?.addRectangle()
+  }
+
   return (
     <div className="main-panel" id="visual-panel">
+      <div>
+        <button onClick={onAddCircle}>Add circle</button>
+        <button onClick={onAddRectangle}>Add Rectangle</button>
+        <FabricJSCanvas className="canvas-panel" onReady={onReady} />
+      </div>
       <div className="bottom right">
         <Stack direction="row">
           <IconButton aria-label="videocam">
